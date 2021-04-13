705d9ddff688bc331130f14bb6329b5d0bfa94b0
C17H35COOH <zhenghaoran97511@gmail.com>
2021-04-13 21:09:47 +0800

    map v1.0

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -17,28 +17,28 @@ public class playerControl : MonoBehaviour {
         if(Input.GetKey(KeyCode.W))
 		{
 			print("w");
-			transform.Translate(Vector3.up * Time.deltaTime * 1);
+			transform.Translate(Vector3.up * Time.deltaTime * 1 * speed);
 		}
 
 		//向下运动——S
 		if(Input.GetKey(KeyCode.S))
 		{
 			print("s");
-			transform.Translate(Vector3.down * Time.deltaTime * 1);
+			transform.Translate(Vector3.down * Time.deltaTime * 1 * speed);
 		}
 
 		//向左运动——A
 		if(Input.GetKey(KeyCode.A))
 		{
 			print("a");
-			transform.Translate(Vector3.left * Time.deltaTime * 1);
+			transform.Translate(Vector3.left * Time.deltaTime * 1 * speed);
 		}
 
 		//向右运动——D
 		if(Input.GetKey(KeyCode.D))
 		{
 			print("d");
-			transform.Translate(Vector3.right * Time.deltaTime * 1);
+			transform.Translate(Vector3.right * Time.deltaTime * 1 *speed);
 		}
         
     }
